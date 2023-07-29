81d0a75b8b0af3a3d2d2e48e3900fbffa5f48a26
angelogro <andre.gutierrez99@gmail.com>
2023-07-29 15:14:01 -0600

    hover animation for navbar

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -3,12 +3,42 @@
     name: "Header",
     data() {
       return {
-        hideMenu: true
+        showMenu: false as Boolean,
+        xPosition: 0 as number,
+        currentWidth: 0 as number,
+        currentHeight: 0 as number
       }
     },
     methods: {
       toggleMenu: function() {
-        this.hideMenu = !this.hideMenu
+        if (this.showMenu === null) {
+          this.showMenu = false
+          return
+        }
+        this.showMenu = !this.showMenu
+      },
+      hideMenu: function() {
+        this.showMenu = false
+      },
+      setHover: function({target}: any) {
+        const anchor = target as HTMLAnchorElement
+        const ul = this.$refs.listRef as HTMLUListElement
+        const navbar = this.$refs.navRef as HTMLElement
+        const { width, height, x: anchorX } = anchor.getBoundingClientRect();
+        const { x: listX } = ul.getBoundingClientRect();
+        if (navbar.getBoundingClientRect().width < 768) return
+        this.currentWidth = width;
+        this.currentHeight = height;
+        this.xPosition = anchorX - listX;
+      }
+    },
+    computed: {
+      getHover: function() {
+        return `
+          --height-anchor: ${this.currentHeight}px;
+          --width-anchor: ${this.currentWidth}px;
+          --x-position: ${this.xPosition}px;
+        `;
       }
     }
   }
@@ -16,7 +46,7 @@
 
 <template>
   <header>
-    <nav>
+    <nav ref="navRef">
       <h1>
         <template class="desktop">
           <a href="#Profile">
@@ -24,17 +54,18 @@
           </a>
         </template>
         <template class="mobile">
-          <button @click="toggleMenu" @blur="toggleMenu">
+          <button @click="toggleMenu" @blur="hideMenu">
             <font-awesome-icon :icon="['fas', 'bars']" />
           </button>
         </template>
       </h1>
-      <ul :class="['menu', {hideMenu}]" >
+      <ul :class="['menu', {showMenu}]" ref="listRef">
+        <li class="hover-anchor" :style="getHover"></li>
         <li>
-          <a href="#Profile">Profile</a>
+          <a @click="setHover" href="#Profile">Profile</a>
         </li>
         <li>
-          <a href="#Projects">Projects</a>
+          <a @click="setHover" href="#Projects">Projects</a>
         </li>
       </ul>
     </nav>
