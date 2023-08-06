6b1c475b15c29c8b19c70fb25e34a226db946a71
angelogro <andre.gutierrez99@gmail.com>
2023-08-06 15:04:27 -0600

    vertical navbar animation implemented

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,51 +1,86 @@
-<script lang="ts">
+<script lang='ts'>
   import { mapState, mapMutations } from 'vuex';
 
   export default {
-    name: "Header",
+    name: 'Header',
     data() {
       return {
-        showMenu: false as boolean,
+        showMenu: true as boolean,
         xPosition: 0 as number,
+        yPosition: 10 as number,
         currentWidth: 0 as number,
         currentHeight: 0 as number,
+        headerHeight: 0 as number,
+        windowWidth: 0 as number,
         pages: [
-          "Profile",
-          "About",
-          "Projects",
-          "Contact"
-        ] as Array<string>,
+          {
+            name: 'Profile',
+            icon: 'user'
+          }, {
+            name: 'About',
+            icon: 'user'
+          }, {
+            name: 'Projects',
+            icon: 'user'
+          }, {
+            name: 'Contact',
+            icon: 'user'
+          }
+        ],
+        anchors: null as null | Array<HTMLAnchorElement>
       }
     },
     methods: {
       ...mapMutations(['setCurrentPage']),
       toggleMenu(): void {
         if (this.showMenu === null) {
-          this.showMenu = false
-          return
+          this.showMenu = false;
+          return;
         }
-        this.showMenu = !this.showMenu
+        this.showMenu = !this.showMenu;
       },
-      hideMenu(): void {
-        this.showMenu = false
-      },
+      hideMenu(): void { this.showMenu = false },
       setHover(): void {
-        const anchors = this.$refs.page as Array<HTMLAnchorElement>
-        const currentAnchor = anchors.find(e => e.innerText === this.currentPage)
-        if (!currentAnchor) return
-        const ul = this.$refs.listRef as HTMLUListElement
-        const navbar = this.$refs.navRef as HTMLElement
-        const { width, height, x: anchorX } = currentAnchor.getBoundingClientRect();
-        const { x: listX } = ul.getBoundingClientRect();
-        if (navbar.getBoundingClientRect().width < 768) return
-        this.currentWidth = width;
-        this.currentHeight = height;
+        if (!this.anchors) return
+        const currentAnchor =  this.anchors.find(e => e.innerText === this.currentPage);
+        if (!currentAnchor) return;
+
+        const ul = this.$refs.listRef as HTMLUListElement;
+        const navbar = this.$refs.navRef as HTMLElement;
+        const {
+          width:anchorWidth,
+          height: anchorHeight,
+          x: anchorX,
+          y: anchorY 
+        } = currentAnchor.getBoundingClientRect();
+
+        const {
+          x: listX,
+          y: listY
+        } = ul.getBoundingClientRect();
+
+        const {
+          height: headerHeight
+        } = navbar.getBoundingClientRect();
+
+        this.currentWidth = anchorWidth;
+        this.currentHeight = anchorHeight;
         this.xPosition = anchorX - listX;
+        this.yPosition = anchorY - listY;
+        this.headerHeight = headerHeight;
+
+        if (this.windowWidth < 768)
+          this.xPosition = 0;
+        else
+          this.yPosition = 0;
       },
       setCurrentPageEvent(event: MouseEvent): void {
         const anchor = event.target as HTMLAnchorElement
         this.setCurrentPage(anchor.innerText)
       },
+      onResize(): void {
+        this.windowWidth = globalThis.innerWidth;
+      }
     },
     computed: {
       getHover(): string {
@@ -53,17 +88,25 @@
           --height-anchor: ${this.currentHeight}px;
           --width-anchor: ${this.currentWidth}px;
           --x-position: ${this.xPosition}px;
+          --y-position: ${this.yPosition}px;
         `;
+      },
+      getHeaderHeight(): string {
+        return `--header-height: ${this.headerHeight}px;`;
       },
       ...mapState(['currentPage'])
     },
     mounted() {
-      const { hash } = globalThis.location
-      this.setCurrentPage(hash.replace("#", ""))
-      this.setHover()
+      this.anchors = this.$refs.page as Array<HTMLAnchorElement>;
+      this.$nextTick(() => {
+        globalThis.addEventListener('resize', this.onResize);
+      })
+      this.onResize();
+      this.setHover();
     },
     watch: {
-      currentPage() { this.setHover() }
+      currentPage() { this.setHover(); },
+      windowWidth() { this.setHover(); }
     }
   }
 </script>
@@ -83,14 +126,19 @@
           </button>
         </template>
       </h1>
-      <ul :class="['menu', {showMenu}]" ref="listRef">
+      <ul :class="['menu', {showMenu}]" ref="listRef" :style="getHeaderHeight">
         <li class="hover-anchor" :style="getHover"></li>
         <template v-for="page in pages">
           <li>
             <a @click="setCurrentPageEvent($event)"
               ref="page"
-              :href="`#${page}`">
-              {{page}}
+              :class="page.name === currentPage? 'active': null"
+              :href="`#${page.name}`">
+              <span>
+                <!-- <font-awesome-icon v-if="windowWidth < 768"
+                :icon="[page.name === currentPage ? 'fas': 'far', page.icon]" /> -->
+                {{page.name}}
+              </span>
             </a>
           </li>
         </template>
@@ -100,5 +148,5 @@
 </template>
 
 <style scoped>
-  @import "./styles.module.scss";
+  @import './styles.module.scss';
 </style>
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import { store } from './store';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faCode, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCode, faBars, faXmark, faUser as faUserSolid} from '@fortawesome/free-solid-svg-icons';
+import { faUser as faUserOutline} from '@fortawesome/free-regular-svg-icons';
 
-library.add(faGithub, faLinkedin, faCode, faBars, faXmark)
+library.add(faGithub, faLinkedin, faCode, faBars, faXmark, faUserOutline, faUserSolid)
 
 createApp(App)
 .use(store)
