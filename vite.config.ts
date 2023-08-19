6c229befdcaa6734eef1a76730cc1a83b3b6939d
angelogro <andre.gutierrez99@gmail.com>
2023-08-19 12:56:22 -0600

    base url changed

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/Portfolio/",
   resolve: {
     alias: [{find: '@', replacement: path.resolve(__dirname, 'src')}]
   },
